ba8e6a1544ecbc815faeaa5ff1a9d7d10d9e9f36
jmoore <josh@glencoesoftware.com>
2022-03-15 13:39:46 -0500

    Add no-op test to keep GH happy

---
+++ tests/test_bioformats2raw.py
@@ -0,0 +1,12 @@
+import pytest
+
+# from ome_zarr.io import ZarrLocation
+# from ome_zarr.reader import Node
+
+# from ome_zarr_metadata.spec import bioformats2raw
+
+
+def test_spec():
+    assert 1
+    with pytest.raises(AssertionError):
+        assert 0
