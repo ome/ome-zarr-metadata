e73bb608d25d106fa28ac9927d61c5e69ad3a544
jmoore <josh@glencoesoftware.com>
2022-03-15 13:35:09 -0500

    scaffold update

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "ome-zarr-metadata"
-copyright = "2022, jmoore"
+copyright = "2022, The Open Microscopy Team"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -246,7 +246,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "ome-zarr-metadata Documentation", "jmoore", "manual")
+    ("index", "user_guide.tex", "ome-zarr-metadata Documentation", "The Open Microscopy Team", "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
     Setup file for ome-zarr-metadata.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.1.5.
+    This file was generated with PyScaffold 4.2.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
